98197617f2d69fac81073b7e00676dcc1f95b7fc
Jonas Siuksteris <siuksterisjonas@gmail.com>
2018-02-08 13:27:25 +0200

    Antras

--- main.cpp
+++ main.cpp
@@ -6,21 +6,15 @@ int main()
 {
     string vardas;
     int lytis = 0;
-    int ilgis = 0;
-    cout << "Lytis: 1 - Moteris, 2 - Vyras" << endl;
+    cout << "Lytis: 0 - Vyras. 1 - Moteris" << endl;
     cin >> lytis;
     cout << "Áveskite savo vardà: ";
     cin >> vardas;
     cout << endl;
-    ilgis = vardas.length();
-    if(lytis == 1)
-        ilgis += 13;
-    else
-        ilgis += 14;
-    for(int i = 0; i < ilgis; i++)
+    for(int i = 0; i < vardas.length()+14-lytis; i++)
         cout << "*";
     cout << endl << "*";
-    for(int i = 0; i < ilgis-2; i++)
+    for(int i = 0; i < vardas.length()+12-lytis; i++)
         cout << " ";
     cout << "*" << endl << "* ";
     if(lytis == 1)
@@ -28,10 +22,10 @@ int main()
     else
         cout << "Sveikas, ";
     cout << vardas << "! *" << endl << "*";
-    for(int i = 0; i < ilgis-2; i++)
+    for(int i = 0; i < vardas.length()+12-lytis; i++)
         cout << " ";
     cout << "*" << endl;
-    for(int i = 0; i < ilgis; i++)
+    for(int i = 0; i < vardas.length()+14-lytis; i++)
         cout << "*";
 
 }
